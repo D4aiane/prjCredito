3e4e816c1e6cc962f9f7af7026ba1f5e441fb7ae
D4aiane <dayanesantos200067@gmail.com>
2020-05-28 22:42:54 +0000

    implementando

---
+++ prjCredito/src/app/credito/credito.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SaldoComponent } from './component/saldo/';
+
+
+
+@NgModule({
+  declarations: [SaldoComponent],
+  imports: [
+    CommonModule
+  ], exports:[
+    SaldoComponent
+  ]
+})
+export class CreditoModule { }
